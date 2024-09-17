503fda0c3ecec059708ac8371f9c3c6a3246e461
RamiroMoralesdev <rjaviermoralesjm@gmail.com>
2024-09-17 19:47:10 -0300

    Estructurando posts series. Para leer con los .mdoc de Keystatic. Pero.... - El foreach con Astro.glob no retorna los archivos. - El panel de Keystatic se rompio

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -6,7 +6,7 @@ export default config({
     kind: 'local',
   },
   collections: {
-    posts: collection({
+    posts: collection({ // Estructura de post Peliculas
       label: 'Posts',
       slugField: 'title',
       path: 'src/posts/peliculas/*',
@@ -20,6 +20,23 @@ export default config({
         image: fields.object({
           url: fields.text({ label: 'URL de la imagen' }),
         }),
+      },
+    }),
+    series: collection({  // Estructura de post Series 
+      label: 'Series',
+      slugField: 'title',
+      path: 'src/posts/series/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Título' } }),
+        description: fields.text({ label: 'Descripción' }),
+        fecha: fields.text({ label: 'Fecha de publicación' }),
+        video: fields.text({ label: 'URL del video' }),
+        image: fields.object({
+          url: fields.text({ label: 'URL de la imagen' }),
+        }),
+        content: fields.markdoc({ label: 'Contenido' }),
+        seasons: fields.text({ label: 'Número de temporadas' }),
       },
     }),
   },
